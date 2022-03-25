4ec76a7b96c4d90a69f96123d45bb746c10ccc34
Nikolai Fomin <kk309@mail.ru>
2022-03-25 11:31:02 +0300

    add counting filters

--- src/components/FilterBlock/FilterCount/FilterCount.tsx
+++ src/components/FilterBlock/FilterCount/FilterCount.tsx
@@ -1,10 +1,27 @@
-import { FC } from "react"
+import { FC, useEffect } from "react"
+import { useTypedSelector } from "../../../store/selectors"
 import { IFilterCountProps } from "./FilterCountTypes"
 
 import "./FilterCount.scss"
 
 const FilterCount: FC<IFilterCountProps> = ({ count }): JSX.Element => {
-  return <div className="FilterCount">{count}</div>
+  const { filters } = useTypedSelector((state) => state.flat)
+
+  const countFilters = (data: (string | number)[]): number => {
+    let count = 0
+
+    data.map((elem: string | number) => {
+      if (elem !== null) count++
+    })
+
+    return count
+  }
+
+  useEffect(() => {
+    countFilters(Object.values(filters))
+  }, [filters])
+
+  return <div className="FilterCount">{countFilters(Object.values(filters))}</div>
 }
 
 export default FilterCount
